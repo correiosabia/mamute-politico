f41d77c8476e8d674a3f821215b8226c7abd653f
JMallone <michels@alunos.utfpr.edu.br>
2026-05-05 14:32:38 -0300

    Adicione mais exemplos no readme

--- mamute_scrappers/camara_crawler/__init__.py
+++ mamute_scrappers/camara_crawler/__init__.py
@@ -3,6 +3,12 @@
 from .parliamentarian import parliamentarian
 from .proposition import proposition
 from .agency import agency
-from .speeches_transcripts import speeches_transcripts
+
+
+def speeches_transcripts(*args, **kwargs):
+    """Import lazily to avoid runpy warning when executing this module with -m."""
+    from .speeches_transcripts import speeches_transcripts as _speeches_transcripts
+
+    return _speeches_transcripts(*args, **kwargs)
 
 __all__ = ["parliamentarian", "proposition", "agency", "speeches_transcripts"]
